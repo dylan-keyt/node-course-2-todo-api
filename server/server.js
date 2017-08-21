0468cf11e7243b3cf9bb87fea17f34eedf244d01
Dylan Keyt <15023426+dylan-keyt@users.noreply.github.com>
2017-08-21 17:06:11 +1000

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -61,8 +62,8 @@ app.post('/users', (req, res) => {
   console.log(req.body);
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
