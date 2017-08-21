888581ed3222511fa056c55db9034002270ffd3b
Dylan Keyt <15023426+dylan-keyt@users.noreply.github.com>
2017-08-21 15:19:48 +1000

    add get todos rout

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
   console.log(req.body);
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({todos});
+  }, err => {
+    res.status(400).send(err);
+  });
+})
+
 app.post('/users', (req, res) => {
   const user = new User({
     email: req.body.email
