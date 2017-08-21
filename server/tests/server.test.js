dc3488fa870ee12037f22dc7f0b0c89e916b40ea
Dylan Keyt <15023426+dylan-keyt@users.noreply.github.com>
2017-08-21 15:35:24 +1000

    test(get): added test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,17 @@ const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 const { User } = require('./../models/user');
 
+const todos = [{
+  text: 'first test todo'
+}, {
+  text: 'second test todo'
+}];
+
 describe('POST /todos', () => {
   beforeEach(done => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+      return Todo.insertMany(todos);
+    }).then(() => done());
   });
 
   it('should create a new todo', (done) => {
@@ -23,7 +31,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -41,10 +49,22 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch(e => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
 
