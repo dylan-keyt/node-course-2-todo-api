3d364a5e7dce28a4a4154acdb1d579b45e248f77
Dylan Keyt <15023426+dylan-keyt@users.noreply.github.com>
2017-08-28 16:48:01 +1000

    test asded test

--- server/server.js
+++ server/server.js
@@ -109,6 +109,14 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.post('/users', (req, res) => {
   const body = _.pick(req.body, ['email', 'password']);
   const user = new User(body);
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -305,5 +305,24 @@ describe('users', () => {
     });
   });
 
+  describe('DELETE /users/me/token', () => {
+    it('should delete a user auth token', (done) => {
+      request(app)
+        .delete(`/users/me/token`)
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          User.findById(users[0]._id).then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => done(e));
+        });
+    });
+
+  });
+
 
 });
